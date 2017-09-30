b86997a9c19698ad5b78f7e8013c0139fb535690
jsonwang <287971051@qq.com.com>
2017-09-30 11:36:41 +0800

    依赖库说明 整理到 NXFramework.podspec中

--- NXFramework/Classes/NXFramework.h
+++ NXFramework/Classes/NXFramework.h
@@ -8,55 +8,6 @@
 
 //当前版本0.1
 
- /*
- 使用说明 XXXX 非常重要
- 要导入
- 1,libsqlite3.0.tbd FMDB使用
- 2,libz.tbd  NSData+NXCategory gzip 使用
- 
- 方法1:
- Targets --> build Settings --> Other Linker Flags  添加 -lz  -lsqlite3
- 方法2:
- Targets --> build phases --> link binary with libraries  加库  libsqlite3.0.tbd  libz.tbd
- 3,要导入SDWebImage ,NXLaunchVC.m 使用
-  
- */
-
-///////////////////////////// pod file set
-/*
- 
- # Uncomment the next line to define a global platform for your project
- source 'https://github.com/CocoaPods/Specs.git'
- platform :ios, '8.0'
- 
- target 'NXNetworkManager' do
- # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
- # use_frameworks!
- # Pods for NXNetworkManager
- 
- pod 'AFNetworking' , '~> 3.1.0'  #必要 === 网络库 同看 https://github.com/shaioz/AFNetworking-AutoRetry
- 
- pod 'Aspects' , '~> 1.4.1' #非必要 === 面向切面编程 https://wereadteam.github.io/2016/06/30/Aspects/
- 
- pod 'FMDB', '~> 2.7.2' #必要 === 数据库
- 
- pod 'MJRefresh', '~> 3.1.12'  #非必要 === 下载刷新组件
- 
- pod 'MLeaksFinder', '~> 1.0.0' #非必要 === 精准 iOS 内存泄露检测工具 https://wereadteam.github.io/2016/02/22/MLeaksFinder/
- 
- pod 'SDAutoLayout', '~> 2.2.0' #非必要
- 
- pod 'SDWebImage', '~> 4.1.0'  #必要
- 
- pod 'SVProgressHUD', :git => 'https://github.com/SVProgressHUD/SVProgressHUD.git'
- 
- end   #非必要
-
- 
- */
-
-
-
 #ifndef NXlib_NXFramework_h
 #define NXlib_NXFramework_h
 
@@ -103,7 +54,6 @@
 #import "NXSensorDetect.h"
 
 // -------------------------- autoLayout -------------------------- //
-//#import "UIView+SDAutoLayout.h"
 
 // -------------------------- Category -------------------------- //
 #import "NSArray+NXCategory.h"
